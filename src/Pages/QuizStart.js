d432b787fc3000104925adc5a4f84fa1f22de943
Pratik <pratikrajaryan@gmail.com>
2021-08-10 15:51:48 +0530

    first commit

---
+++ src/Pages/QuizStart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function QuizStart() {
+  return (
+    <div className="pages">
+     Quiz Start 
+    </div>
+  );
+}
+
+export default QuizStart;
